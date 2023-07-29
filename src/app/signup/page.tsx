d3be8199a465b8edb42193dedf2b6d3094221bfb
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-29 12:59:43 +0200

    import react, axios, useRouter and link

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import Link from "next/link"; 
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
+
 export default function SignupPage() {
     return (
         <div className="flex">
