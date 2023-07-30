30838bc26e7f6f4ba150b8c61a10368df5a2f6b3
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-30 16:11:22 +0200

    add text color black

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
 
             <label htmlFor="email">email</label>
             <input 
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="email" 
             type="text"
             value={user.email}
@@ -32,12 +32,13 @@ export default function LoginPage() {
 
             <label htmlFor="password">password</label>
             <input 
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="password" 
             type="password"
             value={user.password}
             onChange={(e) => setUser({...user, password: e.target.value})}
             placeholder="password"
+
             />
             <button 
             onClick={onLogin}
