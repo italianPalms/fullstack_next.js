1a9d7d14f1cf8da39766f651a3bec224d73dff69
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-31 22:48:46 +0200

    add button and connect it to backend to make sure the token is removed and the user is logged out and sent to the login page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,33 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
 export default function ProfilePage() {
+
+    const router = useRouter()
+    const logout = async () => {
+        try {
+            await axios.get('/api/users/logout')
+            console.log('Logout successful');
+            toast.success('Logout successful')
+            router.push('/login')
+        } catch (error:any) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
-            <button className="border mt-6">Sign out</button>
+            <button
+             onClick={logout}
+             className="bg-blue-500 mt-4 hover:bg-blue-700 text whie font-bold py-2 px-4 rounded">Logout</button>
         </div>
     )
 }
