3f67e97eefb4eb59cefac8954ade352f375b8043
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-01 21:58:48 +0200

    set up logic to fetch user details, a button to trigger it and a link with the id that redirect user to the /profile/'id' page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,11 +2,13 @@
 
 import axios from "axios";
 import Link from "next/link";
+import React, {useState} from "react";
 import {toast} from "react-hot-toast";
 import {useRouter} from "next/navigation";
-export default function ProfilePage() {
 
+export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("nothing")
     const logout = async () => {
         try {
             await axios.get('/api/users/logout')
@@ -20,14 +22,28 @@ export default function ProfilePage() {
 
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className="p-1 mt-3 rounded bg-purple-500">{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+                </Link>}</h2>
             <button
              onClick={logout}
              className="bg-blue-500 mt-4 hover:bg-blue-700 text whie font-bold py-2 px-4 rounded">Logout</button>
+
+            <button
+             onClick={getUserDetails}
+             className="bg-green-800 mt-4 hover:bg-green-900 text whie font-bold py-2 px-4 rounded">Get user details</button>
+             
+
         </div>
     )
 }
