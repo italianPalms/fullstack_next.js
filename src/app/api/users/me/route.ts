294cee356d292c89e7628a32b67e8495143fd1c6
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-04 18:37:13 +0200

    remove empty lines

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -17,6 +17,5 @@ export async function GET(request:NextRequest){
     } catch (error:any) {
         return NextResponse.json({error: error.message}, 
             {status: 400});
-        
     }
 }
