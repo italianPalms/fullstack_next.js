2374a6a483803f6946c51461f89f6be42546b261
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-14 20:15:19 +0200

    adjust comment to make it sure it is a hashed value

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -6,7 +6,7 @@ import bcryptjs from 'bcryptjs';
 export const sendEmail = async({email, emailType, userId}
     :any) => {
         try {
-            //create a hased token
+            //create a hashed token
             const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
            if (emailType === 'VERIFY') {
