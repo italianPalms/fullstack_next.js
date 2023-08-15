3b8211f05520e7f85b8e142f56c53f2a4a7240b5
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-15 20:48:36 +0200

    adjust the email text to make sure user get en url they can copy and paste in their browser

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -5,7 +5,7 @@ import bcryptjs from 'bcryptjs';
 
 export const sendEmail = async({email, emailType, userId}
     :any) => {
-      console.log("sendEmail function called");
+      // console.log("sendEmail function called");
         try {
             //create a hashed token
             const hashedToken = await bcryptjs.hash(userId.toString(), 10)
@@ -33,7 +33,8 @@ export const sendEmail = async({email, emailType, userId}
             from: 'palms@gmail.com',
             to: email, 
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "Verify your mail" : "Reset your password"}</p>`
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your mail" : "Reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
           }
 
           const mailresponse = await transport.sendMail(mailOptions);
