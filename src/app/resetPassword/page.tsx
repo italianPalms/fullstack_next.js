101752ec737dc422a1e585f562bcf0bef6b9ea3a
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-20 23:06:23 +0200

    write the business logic (not api) for reset password

--- src/app/resetPassword/page.tsx
+++ src/app/resetPassword/page.tsx
@@ -9,13 +9,49 @@ export default function resetPassword() {
     const [email, setEmail] = useState("");
     const [loading, setLoading] = useState(false);
 
-    const checkUserExists = async () => {
+    const checkUserExists = async (email: string) => {
         try {
-            
+            const response = await axios.post('/api/users/checkUserExist', {email});
+            return response.data.exists;
+
         } catch (error:any) {
             console.log("Failed to check if user exists", error.message)
             return false;
+        }
+    };
+
+    const sendResetPasswordRequest = async () => {
+        try {
+            setLoading(true);
+
+            if (!email) {
+                toast.error("Please enter your email");
+                return;
+            }
+
+            const userExists = await checkUserExists(email);
+
+            if (!userExists) {
+                toast.error("The user don't exist. Please sign up");
+                return; 
+            }
+
+            const response = await axios.post('/api/usrs/resetPassword', {email});
+
+            if (response.status === 200) {
+                toast.success("Password reset request sent")
+            } else {
+                toast.error('Password reset request failed')
+            }
+
+
+
+        } catch (error:any) {
+            console.log("Password reset request failed", error.message)
+            toast.error(error.message);
             
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -32,7 +68,7 @@ export default function resetPassword() {
         placeholder="email"
         />
 
-        <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Submit</button>
+        <button onClick={() => console.log("Clicked")} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Submit</button>
 
         <Link href="/login" onClick={() => console.log("Redirecting back to log in page")}>Go back to login</Link>
         </div>
