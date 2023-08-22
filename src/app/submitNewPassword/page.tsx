02716502731ccf0fdb7c5902156d16d404c76d6d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-22 21:13:33 +0200

    add page to submit new password

---
+++ src/app/submitNewPassword/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+
+
+export default function submitNewPassword() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-2xl">Enter new password</h1>
+            <label className="mt-3" htmlFor="password">New password</label>
+            <input
+            className="mt-3 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            placeholder="New password"
+            />
+
+            <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Submit</button>
+        </div>
+    )
+}
